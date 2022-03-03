483e1cdf8340e789db0896dd38e75952d6342002
Ikko-T <skyblueboy32@gmail.com>
2022-03-03 12:30:37 +0900

    Check for arguments

--- count_vowels.sh
+++ count_vowels.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]; then
+  echo "Missing sentences..."
+  echo " Usage: $0 <Your sentence here>"
+  echo " Example: $0 This is an Example"
+  exit 1
+fi
+
 str="$*"
 vowels="$(echo "$str"|grep -oi "[aeiou]")"
 count="$(echo -n "$vowels"|wc -l)"
